fd93b323ee2f5b88743e1f4906aaed2e81745510
User-000 <wgfernandes1@gmail.com>
2019-12-17 16:11:14 -0200

    Commit

---
+++ desafioNeogrid/src/com/neogrid/model/Validator.java
@@ -0,0 +1,10 @@
+package com.neogrid.model;
+
+public class Validator {
+
+	public static boolean validaCarga(Caminhao caminhao, Produto produto) {
+		return caminhao.aceitaCarga() && caminhao.podeAceitarProduto(produto)
+		&& caminhao.getDestino().equals(produto.getDestino()) && caminhao.getCapacidade() > produto.getPeso();
+	}
+		
+}
