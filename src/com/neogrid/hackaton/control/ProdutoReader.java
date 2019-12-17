51bdf74911804f7abb54777184c2ce64cb3cb85a
marcelo <schenamarcelo@gmail.com>
2019-12-17 15:43:49 -0300

    reading files

---
+++ src/com/neogrid/hackaton/control/ProdutoReader.java
@@ -0,0 +1,18 @@
+package com.neogrid.hackaton.control;
+
+import java.io.File;
+
+import com.neogrid.model.Produto;
+
+public class ProdutoReader {
+
+	
+	public Produto read(File fileEntry) {
+		Produto produto = new Produto();
+		
+		
+		
+		return produto;
+
+	}
+}
